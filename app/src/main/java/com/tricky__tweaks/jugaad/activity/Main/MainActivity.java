52dad1f68aa1e4907da4ce554a171ea9052d34e6
pratikKataira <pratikkatariya3.pk@gmail.coms>
2020-03-02 12:17:27 -0500

    Splash Activity - splash activity functionality implemented

--- app/src/main/java/com/tricky__tweaks/jugaad/activity/Main/MainActivity.java
+++ app/src/main/java/com/tricky__tweaks/jugaad/activity/Main/MainActivity.java
@@ -2,10 +2,12 @@ package com.tricky__tweaks.jugaad.activity.Main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.tricky__tweaks.jugaad.R;
+import com.tricky__tweaks.jugaad.activity.Splash.SplashActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
     }
 }
--- app/src/main/java/com/tricky__tweaks/jugaad/activity/Splash/SplashActivity.java
+++ app/src/main/java/com/tricky__tweaks/jugaad/activity/Splash/SplashActivity.java
@@ -2,9 +2,12 @@ package com.tricky__tweaks.jugaad.activity.Splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.tricky__tweaks.jugaad.R;
+import com.tricky__tweaks.jugaad.activity.Main.MainActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 3000);
+
     }
 }
