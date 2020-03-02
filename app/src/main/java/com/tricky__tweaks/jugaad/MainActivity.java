eded1011732762903bdabcea3cdec60df525dde4
pratikKataira <pratikkatariya3.pk@gmail.coms>
2020-03-02 09:18:30 -0500

    Initial commit

---
+++ app/src/main/java/com/tricky__tweaks/jugaad/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tricky__tweaks.jugaad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
