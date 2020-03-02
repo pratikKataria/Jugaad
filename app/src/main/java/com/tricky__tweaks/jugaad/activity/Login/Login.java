4739b3e33438480be5f6d83c929f330ee6a372b4
pratikKataira <pratikkatariya3.pk@gmail.coms>
2020-03-02 15:10:26 -0500

    General - inner class changed to lamda expressions

--- app/src/main/java/com/tricky__tweaks/jugaad/activity/Login/Login.java
+++ app/src/main/java/com/tricky__tweaks/jugaad/activity/Login/Login.java
@@ -1,21 +1,13 @@
 package com.tricky__tweaks.jugaad.activity.Login;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
@@ -49,57 +41,48 @@ public class Login extends AppCompatActivity {
 
         init_fields();
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editTextEmail.getText().toString().isEmpty()) {
-                    editTextEmail.setError("should not empty");
-                    editTextEmail.requestFocus();
-                    return;
-                }
-                if (editTextPassword.getText().toString().isEmpty()) {
-                    editTextPassword.setError("should not empty");
-                    editTextPassword.requestFocus();
-                    return;
-                }
-                if (editTextPassword.getText().toString().length() < 6) {
-                    editTextPassword.setError("should to 6 or greater");
-                    editTextPassword.requestFocus();
-                    return;
-                }
-
-                login(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+        loginBtn.setOnClickListener(v -> {
+            if (editTextEmail.getText().toString().isEmpty()) {
+                editTextEmail.setError("should not empty");
+                editTextEmail.requestFocus();
+                return;
             }
+            if (editTextPassword.getText().toString().isEmpty()) {
+                editTextPassword.setError("should not empty");
+                editTextPassword.requestFocus();
+                return;
+            }
+            if (editTextPassword.getText().toString().length() < 6) {
+                editTextPassword.setError("should to 6 or greater");
+                editTextPassword.requestFocus();
+                return;
+            }
+
+            login(editTextEmail.getText().toString(), editTextPassword.getText().toString());
         });
 
     }
 
     private void login(final String email, final String password) {
 
-        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(Login.this , "signup successfull", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(Login.this, MainActivity.class));
-                }
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(Login.this , "signup successfull", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Login.this, MainActivity.class));
             }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                if (e instanceof FirebaseAuthInvalidCredentialsException) {
-                    textViewUserName.setVisibility(View.VISIBLE);
-                    textViewUserName.setText("invalid credentails check password");
-                } else if (e instanceof FirebaseAuthInvalidUserException) {
-                    textViewUserName.setVisibility(View.VISIBLE);
-                    textViewUserName.setText("not regester user please sign up ");
-                } else if (e instanceof FirebaseAuthUserCollisionException) {
-                    textViewUserName.setVisibility(View.VISIBLE);
-                    textViewUserName.setText("already have email");
-                } else if (e instanceof FirebaseAuthWeakPasswordException) {
-                    textViewUserName.setVisibility(View.VISIBLE);
-                    textViewUserName.setText("password is to weak");
-                }
+        }).addOnFailureListener(e -> {
+            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                textViewUserName.setVisibility(View.VISIBLE);
+                textViewUserName.setText("invalid credentails check password");
+            } else if (e instanceof FirebaseAuthInvalidUserException) {
+                textViewUserName.setVisibility(View.VISIBLE);
+                textViewUserName.setText("not regester user please sign up ");
+            } else if (e instanceof FirebaseAuthUserCollisionException) {
+                textViewUserName.setVisibility(View.VISIBLE);
+                textViewUserName.setText("already have email");
+            } else if (e instanceof FirebaseAuthWeakPasswordException) {
+                textViewUserName.setVisibility(View.VISIBLE);
+                textViewUserName.setText("password is to weak");
             }
         });
 
--- app/src/main/java/com/tricky__tweaks/jugaad/activity/Login/SignUp.java
+++ app/src/main/java/com/tricky__tweaks/jugaad/activity/Login/SignUp.java
@@ -1,6 +1,5 @@
 package com.tricky__tweaks.jugaad.activity.Login;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,13 +8,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
@@ -52,65 +45,53 @@ public class SignUp extends AppCompatActivity {
 
         init_fields();
 
-        signupBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editTextEmail.getText().toString().isEmpty()) {
-                    editTextEmail.setError("should not empty");
-                    editTextEmail.requestFocus();
-                    return;
-                }
-                if (editTextPassword.getText().toString().isEmpty()) {
-                    editTextPassword.setError("should not empty");
-                    editTextPassword.requestFocus();
-                    return;
-                }
-                if (editTextPassword.getText().toString().length() < 6) {
-                    editTextPassword.setError("should to 6 or greater");
-                    editTextPassword.requestFocus();
-                    return;
-                }
-
-                signup(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+        signupBtn.setOnClickListener(v -> {
+            if (editTextEmail.getText().toString().isEmpty()) {
+                editTextEmail.setError("should not empty");
+                editTextEmail.requestFocus();
+                return;
             }
+            if (editTextPassword.getText().toString().isEmpty()) {
+                editTextPassword.setError("should not empty");
+                editTextPassword.requestFocus();
+                return;
+            }
+            if (editTextPassword.getText().toString().length() < 6) {
+                editTextPassword.setError("should to 6 or greater");
+                editTextPassword.requestFocus();
+                return;
+            }
+
+            signup(editTextEmail.getText().toString(), editTextPassword.getText().toString());
         });
 
     }
 
     private void signup(final String email, final String password) {
 
-        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(SignUp.this , "signup successfull", Toast.LENGTH_SHORT).show();
-                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(SignUp.this, "login successfull", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(SignUp.this, MainActivity.class));
-                            }
-                        }
-                    });
-                }
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(SignUp.this , "signup successfull", Toast.LENGTH_SHORT).show();
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task1 -> {
+                    if (task1.isSuccessful()) {
+                        Toast.makeText(SignUp.this, "login successfull", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(SignUp.this, MainActivity.class));
+                    }
+                });
             }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                if (e instanceof FirebaseAuthInvalidCredentialsException) {
-                    textViewErrorMessage.setVisibility(View.VISIBLE);
-                    textViewErrorMessage.setText("invalid credentails check password");
-                } else if (e instanceof FirebaseAuthInvalidUserException) {
-                    textViewErrorMessage.setVisibility(View.VISIBLE);
-                    textViewErrorMessage.setText("not regester user please sign up ");
-                } else if (e instanceof FirebaseAuthUserCollisionException) {
-                    textViewErrorMessage.setVisibility(View.VISIBLE);
-                    textViewErrorMessage.setText("already have email");
-                } else if (e instanceof FirebaseAuthWeakPasswordException) {
-                    textViewErrorMessage.setVisibility(View.VISIBLE);
-                    textViewErrorMessage.setText("password is to weak");
-                }
+        }).addOnFailureListener(e -> {
+            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                textViewErrorMessage.setVisibility(View.VISIBLE);
+                textViewErrorMessage.setText("invalid credentails check password");
+            } else if (e instanceof FirebaseAuthInvalidUserException) {
+                textViewErrorMessage.setVisibility(View.VISIBLE);
+                textViewErrorMessage.setText("not regester user please sign up ");
+            } else if (e instanceof FirebaseAuthUserCollisionException) {
+                textViewErrorMessage.setVisibility(View.VISIBLE);
+                textViewErrorMessage.setText("already have email");
+            } else if (e instanceof FirebaseAuthWeakPasswordException) {
+                textViewErrorMessage.setVisibility(View.VISIBLE);
+                textViewErrorMessage.setText("password is to weak");
             }
         });;
 
