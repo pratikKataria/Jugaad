aa557c0c0882a19b015bb81c742ea81aa1b56801
pratikKataira <pratikkatariya3.pk@gmail.coms>
2020-03-02 09:24:09 -0500

    General - Splash Activity Created

---
+++ app/src/main/java/com/tricky__tweaks/jugaad/activity/Splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.tricky__tweaks.jugaad.activity.Splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tricky__tweaks.jugaad.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
