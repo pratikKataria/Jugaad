0f5dda331411bb72a2a0cf482e2f7cd4dafcb7da
pratikKataira <pratikkatariya3.pk@gmail.coms>
2020-03-02 13:42:08 -0500

    Login Activity - Firebase exceptions added

--- app/src/main/java/com/tricky__tweaks/jugaad/activity/Login/Login.java
+++ app/src/main/java/com/tricky__tweaks/jugaad/activity/Login/Login.java
@@ -8,13 +8,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.tricky__tweaks.jugaad.R;
 import com.tricky__tweaks.jugaad.activity.Main.MainActivity;
 
@@ -22,7 +28,7 @@ public class Login extends AppCompatActivity {
 
     private EditText editTextEmail;
     private EditText editTextPassword;
-    private EditText editTextUserName;
+    private TextView textViewUserName;
     private MaterialButton loginBtn;
 
     private FirebaseAuth firebaseAuth;
@@ -30,6 +36,7 @@ public class Login extends AppCompatActivity {
     private void init_fields() {
         editTextEmail = findViewById(R.id.activity_login_et_email);
         editTextPassword = findViewById(R.id.activity_login_et_password);
+        textViewUserName = findViewById(R.id.activity_login_tv_error_message);
         loginBtn = findViewById(R.id.activity_login_mb_login);
 
         firebaseAuth = FirebaseAuth.getInstance();
@@ -69,14 +76,25 @@ public class Login extends AppCompatActivity {
 
     private void login(final String email, final String password) {
 
-        Log.e("LOGIN", email + "  " + password);
-
         firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
                     Toast.makeText(Login.this , "signup successfull", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(Login.this, MainActivity.class));
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    textViewUserName.setText("invalid credentails check password");
+                } else if (e instanceof FirebaseAuthInvalidUserException) {
+                    textViewUserName.setText("not regester user please sign up ");
+                } else if (e instanceof FirebaseAuthUserCollisionException) {
+                    textViewUserName.setText("already have email");
+                } else if (e instanceof FirebaseAuthWeakPasswordException) {
+                    textViewUserName.setText("password is to weak");
                 }
             }
         });
