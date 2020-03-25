0a20e973cbc1063c0899311a54b372c614101435
pratikKataria <pratikatariya3.pk@gmail.com>
2020-03-26 02:11:56 +0530

    JewelleryActivity - onItemClick implemented

--- app/src/main/java/com/tricky__tweaks/jugaad/activity/Main/categories/JewelleryActivity.java
+++ app/src/main/java/com/tricky__tweaks/jugaad/activity/Main/categories/JewelleryActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,8 +16,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.tricky__tweaks.jugaad.Model.EachItemDataModel;
 import com.tricky__tweaks.jugaad.R;
+import com.tricky__tweaks.jugaad.activity.Main.PlaceItemOrderActivity;
 import com.tricky__tweaks.jugaad.adapter.EachCategoryRecyclerAdapter;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 public class JewelleryActivity extends AppCompatActivity {
@@ -41,6 +45,11 @@ public class JewelleryActivity extends AppCompatActivity {
         jewelleryRecyclerAdapter = new EachCategoryRecyclerAdapter(this, jewelleryList);
         jewelleryRv.setLayoutManager(manger);
         jewelleryRv.setAdapter(jewelleryRecyclerAdapter);
+        jewelleryRecyclerAdapter.setOnItemClickListener(position -> {
+            EachItemDataModel eidm = jewelleryList.get(position);
+            startActivity(new Intent(JewelleryActivity.this, PlaceItemOrderActivity.class).putExtra("DATA_MODEL", (Serializable) eidm));
+            Toast.makeText(JewelleryActivity.this, "posittion" + position, Toast.LENGTH_SHORT).show();
+        });
     }
 
     private void populateList() {
