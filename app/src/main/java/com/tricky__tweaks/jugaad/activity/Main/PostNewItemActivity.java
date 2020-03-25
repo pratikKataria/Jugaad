13658b1ef451595c2eaf2ec7563b65dac14d7131
pratikKataria <pratikatariya3.pk@gmail.com>
2020-03-25 17:26:34 +0530

    General - PostNewItemActivity implemented

---
+++ app/src/main/java/com/tricky__tweaks/jugaad/activity/Main/PostNewItemActivity.java
@@ -0,0 +1,16 @@
+package com.tricky__tweaks.jugaad.activity.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tricky__tweaks.jugaad.R;
+
+public class PostNewItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_new_item);
+    }
+}
