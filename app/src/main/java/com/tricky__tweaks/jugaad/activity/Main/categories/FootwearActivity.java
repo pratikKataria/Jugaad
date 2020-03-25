c035a5a66b3862eb376434982394f87996b7b0d6
pratikKataria <pratikatariya3.pk@gmail.com>
2020-03-25 22:22:54 +0530

    FootwearActivity - recycler view data loaded successfully

--- app/src/main/java/com/tricky__tweaks/jugaad/activity/Main/categories/FootwearActivity.java
+++ app/src/main/java/com/tricky__tweaks/jugaad/activity/Main/categories/FootwearActivity.java
@@ -1,16 +1,69 @@
 package com.tricky__tweaks.jugaad.activity.Main.categories;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.tricky__tweaks.jugaad.Model.EachItemDataModel;
 import com.tricky__tweaks.jugaad.R;
+import com.tricky__tweaks.jugaad.adapter.EachCategoryRecyclerAdapter;
+
+import java.util.ArrayList;
 
 public class FootwearActivity extends AppCompatActivity {
+
+    RecyclerView footwearRv;
+    ArrayList<EachItemDataModel> footwearList;
+    EachCategoryRecyclerAdapter footwearAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_footwear);
+
+        footwearList = new ArrayList<>();
+        populateList();
+        init_recyclerView();
     }
+
+    private void init_recyclerView() {
+        footwearRv = findViewById(R.id.footwear_recyclerView);
+        LinearLayoutManager manger = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        footwearAdapter = new EachCategoryRecyclerAdapter(this, footwearList);
+        footwearRv.setLayoutManager(manger);
+        footwearRv.setAdapter(footwearAdapter);
+    }
+
+    private void populateList() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Products/footwear");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot m : dataSnapshot.getChildren()) {
+                    if (m.exists()) {
+                        EachItemDataModel model = m.getValue(EachItemDataModel.class);
+                        footwearList.add(model);
+                        footwearAdapter.notifyDataSetChanged();
+                    }
+                }
+                footwearAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
