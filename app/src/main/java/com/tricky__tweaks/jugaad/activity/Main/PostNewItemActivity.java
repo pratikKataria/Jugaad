6e71ee4567724ffaeb42df225a3fc5c498b23741
pratikKataria <pratikatariya3.pk@gmail.com>
2020-03-25 17:42:53 +0530

    PostNewItemActivity - ui created

--- app/src/main/java/com/tricky__tweaks/jugaad/activity/Main/PostNewItemActivity.java
+++ app/src/main/java/com/tricky__tweaks/jugaad/activity/Main/PostNewItemActivity.java
@@ -12,5 +12,6 @@ public class PostNewItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_new_item);
+
     }
 }
